4fe5f65bc0fd987f32f0977dc9a096fcfb286cf9
Prashanth Raghavan <r.prashanth@gmail.com>
2014-10-24 10:08:30 -0700

    added body parser and handlebars

--- src/server/app.js
+++ src/server/app.js
@@ -1,17 +1,27 @@
 /** Express js server */
 
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+	  _          = require('lodash');
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
+require("./routes/routes.js")(app);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Example app listening at http://%s:%s', host, port);
 
-})
+});
+
